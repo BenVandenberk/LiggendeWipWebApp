d739180f3c9fd16f66ff8f53ec616cfccc065e08
BenVandenberk <ben.vandenberk@hotmail.com>
2016-02-29 17:38:24 +0100

    Ruwe versie bezoeker toernooipagina

--- WEB/src/main/java/be/oklw/bean/bezoeker/ToonToernooiBean.java
+++ WEB/src/main/java/be/oklw/bean/bezoeker/ToonToernooiBean.java
@@ -2,6 +2,7 @@ package be.oklw.bean.bezoeker;
 
 import be.oklw.model.Club;
 import be.oklw.model.Kampioenschap;
+import be.oklw.model.Sponsor;
 import be.oklw.model.Toernooi;
 import be.oklw.service.IKampioenschapService;
 import be.oklw.service.IToernooiService;
@@ -9,6 +10,8 @@ import be.oklw.service.IToernooiService;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import java.util.ArrayList;
+import java.util.List;
 
 @ManagedBean
 @ViewScoped
@@ -21,6 +24,7 @@ public class ToonToernooiBean {
     private Toernooi toernooi;
     private Club club;
     private Kampioenschap kampioenschap;
+    private List<Sponsor[]> sponsorsPerDrie;
 
     public int getToernooiId() {
         return toernooiId;
@@ -38,6 +42,10 @@ public class ToonToernooiBean {
         return kampioenschap;
     }
 
+    public List<Sponsor[]> getSponsorsPerDrie() {
+        return sponsorsPerDrie;
+    }
+
     public void setToernooiId(int toernooiId) {
         boolean veranderd = this.toernooiId != toernooiId;
 
@@ -47,6 +55,19 @@ public class ToonToernooiBean {
             toernooi = toernooiService.getToernooi(toernooiId);
             kampioenschap = toernooi.getKampioenschap();
             club = kampioenschap.getClub();
+
+            sponsorsPerDrie = new ArrayList<Sponsor[]>();
+
+            // Alle resterende sponsors (dus niet de eerste 2) opdelen in groepjes van 3
+
+            Sponsor[] sponsors = new Sponsor[3];
+            for (int i = 0; i < kampioenschap.getSponsors().size(); i++) {
+                if (i % 3 == 0) {
+                    sponsors = new Sponsor[3];
+                    sponsorsPerDrie.add(sponsors);
+                }
+                sponsors[i % 3] = kampioenschap.getSponsors().get(i);
+            }
         }
     }
 }
