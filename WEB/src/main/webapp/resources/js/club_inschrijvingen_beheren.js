ed864a6dd52494cc40c4b559f4b9ea9aea57fe83
BenVandenberk <ben.vandenberk@hotmail.com>
2016-03-14 11:33:06 +0100

    save om naar terug te kunnen

--- WEB/src/main/webapp/resources/js/club_inschrijvingen_beheren.js
+++ WEB/src/main/webapp/resources/js/club_inschrijvingen_beheren.js
@@ -1,19 +1,74 @@
 $(function () {
-    $(".ploegenHeader").click(function () {
+    bindExpandHandlers();
+    restoreState();
+});
+
+function bindExpandHandlers() {
+    $(document).on("click", ".ploegenHeader", function () {
         $(".ploegenContent").slideToggle(500);
         $(".upExpandPloegen").toggle();
         $(".downExpandPloegen").toggle();
+
+        var ploegenZichtbaar = toBoolean($("#frm\\:ploegenZichtbaar").val());
+        $("#frm\\:ploegenZichtbaar").val(!ploegenZichtbaar);
     });
 
-    $(".maaltijdenHeader").click(function () {
+    $(document).on("click", ".maaltijdenHeader", function () {
         $(".maaltijdenContent").slideToggle(500);
         $(".upExpandMaaltijden").toggle();
         $(".downExpandMaaltijden").toggle();
+
+        var maaltijdenZichtbaar = toBoolean($("#frm\\:maaltijdenZichtbaar").val());
+        $("#frm\\:maaltijdenZichtbaar").val(!maaltijdenZichtbaar);
     });
 
-    $(".afrekeningHeader").click(function () {
+    $(document).on("click", ".afrekeningHeader", function () {
         $(".afrekeningContent").slideToggle(500);
         $(".upExpandAfrekening").toggle();
         $(".downExpandAfrekening").toggle();
+
+        var afrekeningZichtbaar = toBoolean($("#frm\\:afrekeningZichtbaar").val());
+        $("#frm\\:afrekeningZichtbaar").val(!afrekeningZichtbaar);
     });
-});
+}
+
+function restoreState() {
+    ploegenState();
+    maaltijdenState();
+    afrekeningState();
+}
+
+function ploegenState() {
+    var ploegenZichtbaar = toBoolean($("#frm\\:ploegenZichtbaar").val());
+    if (ploegenZichtbaar) {
+        $(".ploegenContent").toggle();
+        $(".upExpandPloegen").toggle();
+        $(".downExpandPloegen").toggle();
+    }
+}
+
+function maaltijdenState() {
+    var maaltijdenZichtbaar = toBoolean($("#frm\\:maaltijdenZichtbaar").val());
+    if (maaltijdenZichtbaar) {
+        $(".maaltijdenContent").toggle(0);
+        $(".upExpandMaaltijden").toggle();
+        $(".downExpandMaaltijden").toggle();
+    }
+}
+
+function afrekeningState() {
+    var afrekeningZichtbaar = toBoolean($("#frm\\:afrekeningZichtbaar").val());
+    if (afrekeningZichtbaar) {
+        $(".afrekeningContent").toggle(0);
+        $(".upExpandAfrekening").toggle();
+        $(".downExpandAfrekening").toggle();
+    }
+}
+
+function toBoolean(booleanString) {
+    if (booleanString === "true") {
+        return Boolean(1);
+    } else {
+        return Boolean(0);
+    }
+}
