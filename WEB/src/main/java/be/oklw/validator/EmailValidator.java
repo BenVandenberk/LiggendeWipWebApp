a6e9019a4353b58cce1f16da27865f92ae3be4d4
BenVandenberk <ben.vandenberk@hotmail.com>
2016-04-03 11:56:16 +0200

    emailvalidator

--- WEB/src/main/java/be/oklw/validator/EmailValidator.java
+++ WEB/src/main/java/be/oklw/validator/EmailValidator.java
@@ -13,11 +13,41 @@ import javax.faces.validator.ValidatorException;
 public class EmailValidator implements Validator {
 
     @Override
-    public void validate(FacesContext facesContext, UIComponent uiComponent, Object value) throws ValidatorException {
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        if (value == null) {
+            return;
+        }
+
         String email = (String) value;
 
-        if(StringUtils.isNotBlank(email) && !email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")){
-            throw new ValidatorException(new FacesMessage(""));
+        // Leeg email is ok
+        if (StringUtils.isBlank(email)) {
+            return;
         }
+
+        // Minimum length 6: 'x@y.zz'
+        if (email.length() < 6) {
+            throw new ValidatorException(new FacesMessage("Ongeldige email"));
+        }
+
+        String[] parts = email.split("@");
+
+        // Juist één '@' teken
+        if (parts.length != 2) {
+            throw new ValidatorException(new FacesMessage("Ongeldige email"));
+        }
+
+        String[] partsAfterAt = parts[1].split("\\.");
+
+        // Na de @ minstens één '.'
+        if (partsAfterAt.length < 2) {
+            throw new ValidatorException(new FacesMessage("Ongeldige email"));
+        }
+
+        // 'x@y.zz' -> x is minstens 1 alfanumerisch teken lang
+        if (!StringUtils.isAsciiPrintable(parts[0]) || parts[0].length() < 1) {
+            throw new ValidatorException(new FacesMessage("Ongeldige email"));
+        }
+
     }
 }
