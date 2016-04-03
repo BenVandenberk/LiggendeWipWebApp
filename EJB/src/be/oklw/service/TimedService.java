e05e38d27a890490384d9493620b94b25153f379
BenVandenberk <ben.vandenberk@hotmail.com>
2016-04-03 15:23:44 +0200

    timer contacten

--- EJB/src/be/oklw/service/TimedService.java
+++ EJB/src/be/oklw/service/TimedService.java
@@ -1,14 +1,20 @@
 package be.oklw.service;
 
-import javax.ejb.EJB;
-import javax.ejb.Schedule;
-import javax.ejb.Singleton;
+import javax.annotation.Resource;
+import javax.ejb.*;
 
 @Singleton
 public class TimedService implements ITimedService {
 
+    @Resource
+    TimerService timerService;
+
     @EJB
     IToernooiService toernooiService;
+
+    public void createTimer(long duration) {
+        timerService.createTimer(duration, "Timer contacten gestart");
+    }
 
     /**
      * Elke dag om 00:05:00 worden alle toernooistatussen geüpdate. Als de inschrijfdeadline verstreken is, verandert de status naar 'Inschrijvingen Afgesloten'
@@ -17,4 +23,11 @@ public class TimedService implements ITimedService {
     public void elkeDag() {
         toernooiService.updateInschrijfStatusAlleToernooien();
     }
+
+    @Timeout
+    private void orphanContactenVerwijderen() {
+        // Injecteer de service die je nodig hebt bovenaan en roep hier de method aan die de oprhan contacten verwijderd
+    }
+
+
 }
