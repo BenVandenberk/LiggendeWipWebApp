17540fd6e82f72438ed84cb5f50c9c274bea875f
BenVandenberk <ben.vandenberk@hotmail.com>
2016-04-28 20:11:18 +0200

    Filtering bij Kampioenschappen en Foto's + layoutchanges op deze pagina's

--- WEB/src/main/java/be/oklw/bean/bezoeker/BezoekerFotosBean.java
+++ WEB/src/main/java/be/oklw/bean/bezoeker/BezoekerFotosBean.java
@@ -1,6 +1,8 @@
 package be.oklw.bean.bezoeker;
 
+import be.oklw.model.Club;
 import be.oklw.model.Kampioenschap;
+import be.oklw.service.IClubService;
 import be.oklw.service.IKampioenschapService;
 
 import javax.annotation.PostConstruct;
@@ -9,7 +11,11 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.model.SelectItem;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ManagedBean
 @ViewScoped
@@ -18,18 +24,57 @@ public class BezoekerFotosBean {
     @EJB
     IKampioenschapService kampioenschapService;
 
+    @EJB
+    IClubService clubService;
+
     private List<Kampioenschap> kampioenschappenMetFotos;
+    private List<Kampioenschap> alleKampioenschappenMetFotos;
+
+    private List<Club> alleClubs;
+    private List<SelectItem> clubs;
+    private int selectedClubId = -1;
+
+    public int getSelectedClubId() {
+        return selectedClubId;
+    }
+
+    public void setSelectedClubId(int selectedClubId) {
+        this.selectedClubId = selectedClubId;
+    }
+
+    public List<Club> getAlleClubs() {
+        return alleClubs;
+    }
+
+    public List<SelectItem> getClubs() {
+        return clubs;
+    }
 
     public List<Kampioenschap> getKampioenschappenMetFotos() {
         return kampioenschappenMetFotos;
     }
 
-    public void setKampioenschappenMetFotos(List<Kampioenschap> kampioenschappenMetFotos) {
-        this.kampioenschappenMetFotos = kampioenschappenMetFotos;
+    public void clubChanged(AjaxBehaviorEvent event) {
+        if (selectedClubId < 0) {
+            kampioenschappenMetFotos = new ArrayList<>(alleKampioenschappenMetFotos);
+        } else {
+            kampioenschappenMetFotos = alleKampioenschappenMetFotos.stream().filter(k -> k.getClub().getId() == selectedClubId).collect(Collectors.toList());
+        }
     }
 
     @PostConstruct
     public void init() {
-            kampioenschappenMetFotos = kampioenschapService.getKampioenschappenMetFotos();
+        alleKampioenschappenMetFotos = kampioenschapService.getKampioenschappenMetFotos();
+        kampioenschappenMetFotos = new ArrayList<>(alleKampioenschappenMetFotos);
+
+        // CLUBLIJST VULLEN
+        clubs = new ArrayList<>();
+        alleClubs = clubService.getAllClubs();
+        for (Club c : alleClubs) {
+            clubs.add(new SelectItem(
+                    c.getId(),
+                    c.getNaam()
+            ));
+        }
     }
 }
--- WEB/src/main/java/be/oklw/bean/bezoeker/BezoekerKampioenschapBean.java
+++ WEB/src/main/java/be/oklw/bean/bezoeker/BezoekerKampioenschapBean.java
@@ -1,13 +1,19 @@
 package be.oklw.bean.bezoeker;
 
+import be.oklw.model.Club;
 import be.oklw.model.Kampioenschap;
+import be.oklw.service.IClubService;
 import be.oklw.service.IKampioenschapService;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.model.SelectItem;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ManagedBean
 @ViewScoped
@@ -16,8 +22,18 @@ public class BezoekerKampioenschapBean {
     @EJB
     IKampioenschapService kampioenschapService;
 
+    @EJB
+    IClubService clubService;
+
     private List<Kampioenschap> kampioenschappenVerleden;
     private List<Kampioenschap> kampioenschappenToekomst;
+
+    private List<Kampioenschap> alleKampioenschappenToekomst;
+    private List<Kampioenschap> alleKampioenschappenVerleden;
+
+    private List<Club> alleClubs;
+    private List<SelectItem> clubs;
+    private int selectedClubId = -1;
 
     public List<Kampioenschap> getKampioenschappenVerleden() {
         return kampioenschappenVerleden;
@@ -27,9 +43,49 @@ public class BezoekerKampioenschapBean {
         return kampioenschappenToekomst;
     }
 
+    public int getSelectedClubId() {
+        return selectedClubId;
+    }
+
+    public void setSelectedClubId(int selectedClubId) {
+        this.selectedClubId = selectedClubId;
+    }
+
+    public List<Club> getAlleClubs() {
+        return alleClubs;
+    }
+
+    public List<SelectItem> getClubs() {
+        return clubs;
+    }
+
+    public void clubChanged(AjaxBehaviorEvent event) {
+        // Twee lijsten filteren. De id waarde voor geen filtering is -1
+        if (selectedClubId < 0) {
+            kampioenschappenToekomst = new ArrayList<>(alleKampioenschappenToekomst);
+            kampioenschappenVerleden = new ArrayList<>(alleKampioenschappenVerleden);
+        } else {
+            kampioenschappenToekomst = alleKampioenschappenToekomst.stream().filter(k -> k.getClub().getId() == selectedClubId).collect(Collectors.toList());
+            kampioenschappenVerleden = alleKampioenschappenVerleden.stream().filter(k -> k.getClub().getId() == selectedClubId).collect(Collectors.toList());
+        }
+    }
+
     @PostConstruct
     public void init() {
-        kampioenschappenToekomst = kampioenschapService.getKampioenschappen(false);
-        kampioenschappenVerleden = kampioenschapService.getKampioenschappen(true);
+        alleKampioenschappenToekomst = kampioenschapService.getKampioenschappen(false);
+        alleKampioenschappenVerleden = kampioenschapService.getKampioenschappen(true);
+
+        kampioenschappenToekomst = new ArrayList<>(alleKampioenschappenToekomst);
+        kampioenschappenVerleden = new ArrayList<>(alleKampioenschappenVerleden);
+
+        // CLUBLIJST VULLEN
+        clubs = new ArrayList<>();
+        alleClubs = clubService.getAllClubs();
+        for (Club c : alleClubs) {
+            clubs.add(new SelectItem(
+                    c.getId(),
+                    c.getNaam()
+            ));
+        }
     }
 }
